3a932bed4ce811bf3e66171e2681cc771610c94b
Farid Jafarlee <placeboy.jafaroff@gmail.com>
2017-05-26 17:21:39 +0400

    added custem message event

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,8 @@ var socket = io();
 socket.on('connect', function () {
     console.log("You've connected via socket-io !");
 });
+
+socket.on('message', function (message) {
+    console.log('new Message !');
+    console.log(message.text);
+});
